dd060c6b3d77323000e8491843ea298b15b8cd40
Tom1989 <thomas.walciszewski@gmail.com>
2023-11-13 18:04:08 +0100

    Spring Security updated- Fixed servlet dispacher path issue between MVC and H2 db path

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,17 +2,14 @@ package tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 import tacos.AppUser;
 import tacos.data.UserRepository;
 
@@ -27,38 +24,42 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepo) {
-     return username -> {
-     AppUser user = userRepo.findByUsername(username);
-     if (user != null) return user;
-     throw new UsernameNotFoundException("User '" + username + "' not found");
-     };
+        return username -> {
+            AppUser user = userRepo.findByUsername(username);
+            if (user != null) return user;
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
     }
 
-
-   
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-            .authorizeRequests(authz -> authz
-                .requestMatchers("/design", "/orders").hasRole("USER")
-                .requestMatchers("/", "/**").permitAll()
+        http
+            .authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers(new AntPathRequestMatcher("/design", "GET")).hasRole("USER")
+                    .requestMatchers(new AntPathRequestMatcher("/orders", "GET")).hasRole("USER")
+                    .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll() // Permit H2 console access
+                    .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
             )
-            .formLogin(form -> form
-                .loginPage("/login") // if you are providing a custom login page
-                .defaultSuccessUrl("/design", true) // specify the default success URL after login
-                .permitAll() // allows everyone to see the login page. Don't require auth for login page
+            .formLogin(formLogin ->
+                formLogin
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/design", true)
+                    .permitAll()
             )
             .logout(logout -> logout
-                .logoutSuccessUrl("/?logout") // specify the default page after logout
-                // more logout configuration
+                    .logoutUrl("/logout")
+                .logoutSuccessUrl("/?logout")
             )
-            // You may want to configure CSRF, rememberMe, etc...
-            .build();
+            // Other configurations...
+            ;
+
+        // Disable CSRF for H2 console
+        http.csrf().disable();
+        // Allow use of frame to same origin urls (needed for H2-console)
+        http.headers().frameOptions().sameOrigin();
+
+        return http.build();
     }
 
-
-    }
-
-
-
-
+}
